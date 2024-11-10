780e39a4827a04add71a8beb1b4eceec7ca74f45
IRFANSANDI <irfansandi4693@gmail.com>
2024-11-10 13:52:05 +0700

    Irfan sandi 24241019
    tugas ngoding

---
+++ Irfan Sandi/04-InputUser/main.py
@@ -0,0 +1,7 @@
+# input dari user
+data = input("masukkan data : ")
+print("data : ", data, "Tipe : ", type(data))
+
+# jika adalah adalah angka maka casting 
+angka = float(input( "masukkan angka : "))
+print("data : ", angka, "tipe : ", type (angka))
